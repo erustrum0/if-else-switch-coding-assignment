efd6e17aa2da98bab91d80cb0a81552f36d34794
erustrum0 <157773321+erustrum0@users.noreply.github.com>
2024-03-13 03:50:18 +0000

    done

--- src/4.cpp
+++ src/4.cpp
@@ -2,9 +2,35 @@
 #include <cmath>
 using namespace std;
 
+void segfault() {
+    // hee hee
+    int* pointer = nullptr;
+    *pointer = -0;
+}
+
+int calculate(int op, int numA, int numB) {
+    switch (op) {
+        case 1:
+            return numA + numB;
+            break;
+        case 2:
+            return numA - numB;
+            break;
+        case 3:
+            return numA * numB;
+            break;
+        case 4:
+            return numA / numB;
+            break;
+    }
+    segfault();
+    return 0;
+}
+
 int main() {
     int numA;
     int numB;
+    int result;
     int op;
     bool progRunning = true;
 
@@ -12,24 +38,33 @@ int main() {
         cout << "Enter a number 1-4 to select an operation (0 to quit):\n\t1 - Addition\n\t2 - Subtraction\n\t3 - Multiplication\n\t4 - Division\n\n>> ";
         cin >> op;
         switch (op) {
+            // Fallthrough case my beloved
             case 1:
-                //Addition
-                break;
+                // Addition
             case 2:
-                //Subtraction
-                break;
+                // Subtraction
             case 3:
-                //Multiplication
-                break;
+                // Multiplication
             case 4:
-                //Division
+                // Division
+                cout << "Please enter the first operator: ";
+                cin >> numA;
+                cout << "Please enter the second operator: ";
+                cin >> numB;
+                result = calculate(op, numA, numB);
+                cout << "\nThe result of your calculation is " << result << ".\n\n";
                 break;
             case 0:
-                //Division
+                // Break from loop
                 progRunning = false;
+                break;
+            case 139:
+                // Segfault because why not
+                segfault();
                 break;
             default:
                 cout << "Bad input!\n";
         }
     }
+    return 0;
 }
